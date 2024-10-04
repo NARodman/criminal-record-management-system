454dd28f1465fd6a283089665b9940a0ae1c709b
NARodman <ninaalanier8@gmail.com>
2024-10-03 22:03:52 -0400

    Create CriminalRecordsManager.java

---
+++ CriminalRecordsManager.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * CriminalRecordsManager
+ * 
+ * A command-line application to manage criminal records.
+ * Allows users to add, view, edit, and delete records stored in records.txt.
+ * 
+ * Records are stored in the format:
+ * Name | Crime | DateOfCrime | Sentence
+ * 
+ * Example:
+ * John Doe | Theft | 2023-05-15 | 2 years imprisonment
+ * 
+ * @Nina Lanier
+ */
+
+ 
